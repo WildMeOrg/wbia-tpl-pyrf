8784eaf8a1db70178d9257733a915f2a18660b8a
joncrall <crallj@rpi.edu>
2017-07-20 15:29:37 -0400

    localized old vtool funcs

--- run_tests.py
+++ run_tests.py
@@ -1,12 +1,76 @@
 #!/usr/bin/env python2.7
 from __future__ import absolute_import, division, print_function
-from os.path import join, split
+from os.path import join, split, exists
 from pyrf import Random_Forest_Detector
 import utool as ut
 
 
 TEST_DATA_DETECT_URL = 'https://lev.cs.rpi.edu/public/data/testdata_detect.zip'
 TEST_DATA_MODEL_URL = 'https://lev.cs.rpi.edu/public/models/rf.zip'
+
+
+# Parallel code for resizing many images
+def _resize_worker(gfpath, new_gfpath, new_size):
+    """ worker function for parallel generator """
+    import vtool as vt
+    img = vt.imread(gfpath)
+    new_img = vt.resize(img, new_size)
+    vt.imwrite(new_gfpath, new_img)
+    return new_gfpath
+
+
+def resize_imagelist_generator(gpath_list, new_gpath_list, newsize_list, **kwargs):
+    """ Resizes images and yeilds results asynchronously  """
+    # Compute and write thumbnail in asychronous process
+    kwargs['force_serial'] = kwargs.get('force_serial', True)
+    kwargs['ordered']      = kwargs.get('ordered', True)
+    arg_iter = zip(gpath_list, new_gpath_list, newsize_list)
+    arg_list = list(arg_iter)
+    return ut.generate2(_resize_worker, arg_list, **kwargs)
+
+
+def resize_imagelist_to_sqrtarea(gpath_list, new_gpath_list=None,
+                                 sqrt_area=800, output_dir=None,
+                                 checkexists=True,
+                                 **kwargs):
+    """ Resizes images and yeilds results asynchronously  """
+    import vtool as vt
+    target_area = sqrt_area ** 2
+    # Read image sizes
+    gsize_list = [vt.open_image_size(gpath) for gpath in gpath_list]
+    # Compute new sizes which preserve aspect ratio
+    newsize_list = [vt.ScaleStrat.area(target_area, wh) for wh in gsize_list]
+    if new_gpath_list is None:
+        # Compute names for the new images if not given
+        if output_dir is None:
+            # Create an output directory if not specified
+            output_dir      = 'resized_sqrtarea%r' % sqrt_area
+        ut.ensuredir(output_dir)
+        size_suffixs =  ['_' + repr(newsize).replace(' ', '') for newsize in newsize_list]
+        from os.path import basename
+        old_gnames = [basename(p) for p in gpath_list]
+        new_gname_list = [ut.augpath(p, suffix=s)
+                          for p, s in zip(old_gnames, size_suffixs)]
+        new_gpath_list = [join(output_dir, gname) for gname in new_gname_list]
+        new_gpath_list = list(map(ut.unixpath, new_gpath_list))
+    assert len(new_gpath_list) == len(gpath_list), 'unequal len'
+    assert len(newsize_list) == len(gpath_list), 'unequal len'
+    # Evaluate generator
+    if checkexists:
+        exists_list = list(map(exists, new_gpath_list))
+        gpath_list_ = ut.filterfalse_items(gpath_list, exists_list)
+        new_gpath_list_ = ut.filterfalse_items(new_gpath_list, exists_list)
+        newsize_list_ = ut.filterfalse_items(newsize_list, exists_list)
+    else:
+        gpath_list_ = gpath_list
+        new_gpath_list_ = new_gpath_list
+        newsize_list_ = newsize_list
+    generator = resize_imagelist_generator(gpath_list_, new_gpath_list_,
+                                           newsize_list_, **kwargs)
+    for res in generator:
+        pass
+    #return [res for res in generator]
+    return new_gpath_list
 
 
 def test_pyrf():
@@ -53,7 +117,6 @@ def test_pyrf():
     #=================================
 
     # Get input images
-    import vtool as vt
     big_gpath_list = ut.list_images(test_path, fullpath=True, recursive=False)
     print(big_gpath_list)
     # Resize images to standard size
@@ -61,10 +124,10 @@ def test_pyrf():
         big_gpath_list = big_gpath_list[0:8]
     #big_gpath_list = big_gpath_list[0:8]
     output_dir = join(test_path, 'resized')
-    std_gpath_list = vt.resize_imagelist_to_sqrtarea(big_gpath_list,
-                                                     sqrt_area=800,
-                                                     output_dir=output_dir,
-                                                     checkexists=True)
+    std_gpath_list = resize_imagelist_to_sqrtarea(big_gpath_list,
+                                                  sqrt_area=800,
+                                                  output_dir=output_dir,
+                                                  checkexists=True)
     dst_gpath_list = [join(detect_path, split(gpath)[1]) for gpath in std_gpath_list]
     #ut.view_directory(test_path)
     #ut.view_directory('.')
