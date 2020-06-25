12b0e4c1ea156b8edccf6765d3006aecd783e974
Jason Parham <bluemellophone@gmail.com>
2020-06-24 22:10:03 -0700

    Updates for build

--- pyrf/pyrf_helpers.py
+++ pyrf/pyrf_helpers.py
@@ -75,8 +75,8 @@ def _extract_np_array(size_list, ptr_list, arr_t, arr_dtype, arr_dim):
 
 def _load_c_shared_library(METHODS):
     """ Loads the pyrf dynamic library and defines its functions """
-    root_dir = realpath(join('..', dirname(__file__)))
-    libname = 'libpyrf'
+    root_dir = realpath(join(dirname(__file__), 'lib'))
+    libname = 'pyrf'
     rf_clib, def_cfunc = ctypes_interface.load_clib(libname, root_dir)
     # Load and expose methods from lib
     for method in METHODS.keys():
